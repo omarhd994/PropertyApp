b9f2070d6e12ca7205993454656889f94cc1fcca
omar <omarhd994@gmail.com>
2025-01-11 00:04:09 +0100

    App created from Figma design

---
+++ app/(root)/properties/[id].jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
+import React from 'react'
+
+const Property = () => {
+    const { id } = useLocalSearchParams;
+  return (
+    <View>
+      <Text>Property {id}</Text>
+    </View>
+  )
+}
+
+export default Property;
---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+import { Link } from 'expo-router'
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10">Welcome to my Mobile App</Text>
+      <Link href="/sing-in">Sing In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
